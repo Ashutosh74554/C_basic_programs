bc2e411e13686177ffbb010fda06c8cedfb7504a
Ashutosh74554 <ashutoshdash1437@gmail.com>
2021-01-05 21:16:33 +0530

    Re-uploaded on 5-01-2021

---
+++ alphabet pattern.c
@@ -0,0 +1,30 @@
+//alphabet pattern
+#include<stdio.h>
+void main()
+{
+	int n,i,j,k;
+	char x='A';
+	printf("Enter number of rows: ");
+	scanf("%d", &n);
+	for (i=0;i<=n;i++)
+	{
+		for(j=n;j>=i;j--)
+		{
+			printf("%c", x++);
+		}
+    	for(k=1;k<(2*i);k++)
+	    {
+	    	printf(" ");
+	   	}
+		for(j=1;j<=n;j++)
+		{
+			if(i==0)
+			    x--;
+			while(x>='B')
+		    	printf("%c", --x);
+		}
+		x='A';
+		printf("\n");        
+	}
+	
+}
